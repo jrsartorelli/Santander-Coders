195ada725227bf2473a182c930b5129982a2ffc8
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-12-21 23:18:52 -0300

    Exercício 21-12-22

--- PROGRAMACAO ORIENTADA A OBJETOS II/Aula 21-12-22/src/Sistema.java
+++ PROGRAMACAO ORIENTADA A OBJETOS II/Aula 21-12-22/src/Sistema.java
@@ -6,25 +6,21 @@ public class Sistema {
     public static void main(String[] args) {
         Turma<Aluno> Java = new Turma<>();
         Professor professorJava = new Professor("Kadu");
-        Aluno alunoJava1 = new AlunoJava("José");
-        Aluno alunoJava2 = new AlunoJava("Victor");
-        Aluno alunoJava3 = new AlunoJava("Gabriel");
+        Aluno[] alunosJava = new AlunoJava[]{new AlunoJava("José"), new AlunoJava("Victor"), new AlunoJava("Gabriel")};
 
         Turma<Aluno> JavaScript = new Turma<>();
         Professor professorJavaScript = new Professor("Borges");
-        Aluno alunoJS1 = new AlunoJS("Pedro");
-        Aluno alunoJS2 = new AlunoJS("Bruna");
-        Aluno alunoJS3 = new AlunoJS("Marayah");
+        Aluno[] alunosJS = new AlunoJS[]{new AlunoJS("Pedro"), new AlunoJS("Bruna"), new AlunoJS("Marayah")};
 
         Java.setProfessor(professorJava);
-        incluirAlunoTurma(Java, alunoJava1);
-        incluirAlunoTurma(Java, alunoJava2);
-        incluirAlunoTurma(Java, alunoJava3);
+        for (Aluno aluno : alunosJava) {
+            incluirAlunoTurma(Java, aluno);
+        }
 
         JavaScript.setProfessor(professorJavaScript);
-        incluirAlunoTurma(JavaScript, alunoJS1);
-        incluirAlunoTurma(JavaScript, alunoJS2);
-        incluirAlunoTurma(JavaScript, alunoJS3);
+        for (Aluno aluno : alunosJS) {
+            incluirAlunoTurma(JavaScript, aluno);
+        }
 
         Java.ordenaLista();
         JavaScript.ordenaLista();

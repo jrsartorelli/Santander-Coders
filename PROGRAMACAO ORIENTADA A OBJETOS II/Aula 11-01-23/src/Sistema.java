653c8fb56125c52b6f94331ce43358fbaa3bf2e1
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2023-01-13 20:16:07 -0300

    Exercício 11-01-23 Princípios SOLID

--- PROGRAMACAO ORIENTADA A OBJETOS II/Aula 11-01-23/src/Sistema.java
+++ PROGRAMACAO ORIENTADA A OBJETOS II/Aula 11-01-23/src/Sistema.java
@@ -2,10 +2,13 @@ public class Sistema {
     public static void main(String[] args) {
         LivrariaService livraria = new LivrariaServiceImplement();
 
-        Livro produto = new Livro("Java", 100, 3,
+        Produto produto = new Livro("Java", 100, 3,
                 new String[]{"Programação", "TI"}, "Escritor", "Editora");
+        Filme filme = new Filme("Esqueceram de Mim", 20, 2, "MGM",
+                new String[]{"Steven Spielberg"}, new String[]{"Comédia"}, new String[]{"Produtores"});
 
         livraria.adicionarProduto(produto);
+        livraria.adicionarProduto(filme);
 
     }
 }
